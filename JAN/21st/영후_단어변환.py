c2b7b8d883f0c3808a9ae4006981e65c19cc9b8b
yeoncho <yeoncho@student.42seoul.kr>
2021-01-21 22:02:06 +0900

    yeonghu

---
+++ JAN/21st/영후_단어변환.py
@@ -0,0 +1,53 @@
+import sys
+
+begin = 'hit'
+target = 'cog'
+
+words = ["hot", "dot", "dog", "lot", "log", "cog"]
+
+
+def valid(str1, str2) :
+	cnt = 0
+	for i in range(len(str1)) :
+		if str1[i] != str2[i] :
+			cnt += 1
+	if cnt == 1 :
+		return 1
+	else :
+		return 0
+
+def solution(begin, target, words):
+	stk = []
+	min_value = 51
+	
+	for i, content in enumerate(words) :
+		if valid(begin, content) :
+			stk.append((1 << i, content, 1))    # 비트맵으로 방문 표시했음
+
+
+	while stk :
+		bitfield, word, cnt = stk.pop()
+
+		if word == target :
+			min_value = min(min_value, cnt)	
+
+		for i, content in enumerate(words) :
+			if (bitfield & 1 << i) == 0 and valid(word, content) :
+				stk.append(((bitfield | 1 << i), content, cnt + 1))
+
+		if not stk :
+			break
+
+	if min_value == 51 :
+		return 0
+	
+	return min_value
+
+print(solution(begin, target, words))
+		
+
+	
+	
+
+	
+	
