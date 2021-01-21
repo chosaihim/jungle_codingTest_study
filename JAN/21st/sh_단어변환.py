2d2609ad38779004b818be469c95bfbc7ee964c2
chosaihim <chosaihim@gmail.com>
2021-01-21 19:57:11 +0900

    Right version of the code! previous file was wrong one

--- JAN/21st/sh_단어변환.py
+++ JAN/21st/sh_단어변환.py
@@ -1,40 +1,32 @@
-words  = ["hot", "dot", "dog", "lot", "log", "cog"]
-begin  = "hit"
-target = "cog"
-
 from collections import deque
 
 def solution(begin, target, words): 
     
-
+    if target not in words: return 0
+    
     def bfs(begin, target, words):
-        visited = []    # 방문한 곳을 기록
-        queue = []  # 큐에 시작점을 줄 세움
-
-        depth = 0
+        
+        queue = []
+        depth = 0       
+        max_depth = len(words)
         
         queue.append([begin,0])
-
-        while queue:    # 큐가 빌떄까지 탐색을 계속
+        while queue:
             
-            comp, depth = queue.pop(0)
+            comp, depth = queue.pop(0)            
 
-            if ((comp[1] == target[1]) and (comp[2] == target[2])) or ((comp[1] == target[1]) and (comp[2] == target[2])) or ((comp[1] == target[1]) and (comp[2] == target[2])):
-                depth = depth + 1
-                return depth
-
+            if comp == target:    return depth            
+            if depth > max_depth: return 0
+            
             for word in words:
+                diff = 0
                 
-                if (comp[1] == word[1]) and (comp[2] == word[2]):
+                for i in range(len(word)):
+                    if comp[i] != word[i]:
+                        diff += 1
+                if diff == 1:
                     queue.append([word,depth+1]);
-                
-                elif (comp[0] == word[0]) and (comp[2] == word[2]):
-                    queue.append([word,depth+1]);
-
-                elif (comp[0] == word[0]) and (comp[1] == word[1]):
-                    queue.append([word,depth+1])
-
+                        
         return 0
-
-    answer = bfs(begin,target,words)
-    return answer
+        
+    return bfs(begin,target,words)
