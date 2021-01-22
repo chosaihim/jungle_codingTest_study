c7ce82fddacb12e7e8a1647ea7f52c6e6e4afee6
yeoncho <yeoncho@student.42seoul.kr>
2021-01-22 10:45:07 +0900

    yeonghu

---
+++ JAN/22nd/영후_이진변환반복.py
@@ -0,0 +1,20 @@
+s = "110010101001"
+
+
+def solution(s):
+	cnt1 = 0
+	cnt2 = 0
+	while True :
+		for nbr in s :
+			if nbr == '0' :
+				cnt2 += 1
+		s = s.replace("0", "")
+		s = format(len(s), 'b')
+		cnt1 += 1
+
+		if s == '1' :
+			break
+
+	print([cnt1, cnt2])
+
+solution(s)
