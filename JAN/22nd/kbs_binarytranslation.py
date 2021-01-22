a1e821b0fc683814951f4d30066f376dc83447b7
jewerlykim <jsjs21good@gmail.com>
2021-01-22 10:18:07 +0900

    kbs_complete

---
+++ JAN/22nd/kbs_binarytranslation.py
@@ -0,0 +1,31 @@
+import sys
+
+zero_count = 0
+translation_cnt = 0
+def translation(s):
+    global zero_count
+    global translation_cnt
+
+    if s == '1':
+        return 
+    translation_cnt += 1
+    length = 0
+    for word in s:
+        if word == '1':
+            length += 1
+        else:
+            zero_count += 1
+    translation(bin(length)[2:])
+    
+
+
+def solution(s):
+    global zero_count
+    global translation_cnt
+    translation(s)
+
+    answer = [translation_cnt, zero_count]
+    print(answer)
+    return answer
+
+solution("110010101001")
