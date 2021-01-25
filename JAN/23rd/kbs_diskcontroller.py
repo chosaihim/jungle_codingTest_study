179f6c139fa241ca2328cab80c131dcdc452d98a
jewerlykim <jsjs21good@gmail.com>
2021-01-25 10:59:30 +0900

    210125 farest node

--- JAN/23rd/kbs_diskcontroller.py
+++ JAN/23rd/kbs_diskcontroller.py
@@ -5,7 +5,7 @@ def solution(jobs):
     sum_time = 0
     curr_time = 0
     jobs_len = len(jobs)
-    jobs = sorted(jobs, key=lambda x: x[1])
+    jobs = sorted(jobs, key=lambda x: x[1]) # 소요시간이 작은 순으로 정렬. 
     while len(jobs) != 0:
         for i in range(len(jobs)):
             if jobs[i][0] <= curr_time:
@@ -13,7 +13,7 @@ def solution(jobs):
                 sum_time += curr_time - jobs[i][0]
                 jobs.pop(i)
                 break
-            if i == len(jobs) - 1:
+            if i == len(jobs) - 1: # 현재 시간에 아직 작업이 없을 경우 시간을 증가시켜줌
                 curr_time += 1
 
     answer = sum_time // jobs_len
