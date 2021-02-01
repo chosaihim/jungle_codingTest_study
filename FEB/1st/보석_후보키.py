3e70731e95bb28b89ee33a13f280ee641c3345e2
jewerlykim <jsjs21good@gmail.com>
2021-02-01 12:36:13 +0900

    kbs_candidate

--- FEB/1st/보석_후보키.py
+++ FEB/1st/보석_후보키.py
@@ -27,7 +27,7 @@ def solution(relation):
         print(havetocheck_list)
     else:
         return answer
-        
+
     if len(column_list) >= 3:
         three_combilist = list(combinations(column_list,3))
         for x, y, z in three_combilist:
@@ -46,6 +46,101 @@ def solution(relation):
                     havetocheck_list.append({x,y,z})
     else:
         return answer
+
+    if len(column_list) >= 4:
+        three_combilist = list(combinations(column_list,4))
+        for x, y, z, a in three_combilist:
+            flag = False
+            for check in havetocheck_list:
+                if check.issubset({x,y,z,a}):
+                    flag = True
+            if flag:
+                continue
+            else:
+                three_set = set()
+                for i in range(row):
+                    three_set.add((relation[i][x],relation[i][y],relation[i][z],relation[i][a]))
+                if len(three_set) == row:
+                    answer += 1
+                    havetocheck_list.append({x,y,z,a})
+    else:
+        return answer        
+
+    if len(column_list) >= 5:
+        three_combilist = list(combinations(column_list,5))
+        for x, y, z, a, b in three_combilist:
+            flag = False
+            for check in havetocheck_list:
+                if check.issubset({x,y,z,a,b}):
+                    flag = True
+            if flag:
+                continue
+            else:
+                three_set = set()
+                for i in range(row):
+                    three_set.add((relation[i][x],relation[i][y],relation[i][z],relation[i][a], relation[i][b]))
+                if len(three_set) == row:
+                    answer += 1
+                    havetocheck_list.append({x,y,z,a,b})
+    else:
+        return answer    
+
+    if len(column_list) >= 6:
+        three_combilist = list(combinations(column_list,6))
+        for x, y, z, a, b, c in three_combilist:
+            flag = False
+            for check in havetocheck_list:
+                if check.issubset({x,y,z,a,b,c}):
+                    flag = True
+            if flag:
+                continue
+            else:
+                three_set = set()
+                for i in range(row):
+                    three_set.add((relation[i][x],relation[i][y],relation[i][z],relation[i][a], relation[i][b], relation[i][c]))
+                if len(three_set) == row:
+                    answer += 1
+                    havetocheck_list.append({x,y,z,a,b,c})
+    else:
+        return answer    
+
+    if len(column_list) >= 7:
+        three_combilist = list(combinations(column_list,7))
+        for x, y, z, a, b, c, d in three_combilist:
+            flag = False
+            for check in havetocheck_list:
+                if check.issubset({x,y,z,a,b,c,d}):
+                    flag = True
+            if flag:
+                continue
+            else:
+                three_set = set()
+                for i in range(row):
+                    three_set.add((relation[i][x],relation[i][y],relation[i][z],relation[i][a], relation[i][b], relation[i][c], relation[i][d]))
+                if len(three_set) == row:
+                    answer += 1
+                    havetocheck_list.append({x,y,z,a,b,c,d})
+    else:
+        return answer 
+
+    if len(column_list) >= 8:
+        three_combilist = list(combinations(column_list,8))
+        for x, y, z, a, b, c, d, e in three_combilist:
+            flag = False
+            for check in havetocheck_list:
+                if check.issubset({x,y,z,a,b,c,d,e}):
+                    flag = True
+            if flag:
+                continue
+            else:
+                three_set = set()
+                for i in range(row):
+                    three_set.add((relation[i][x],relation[i][y],relation[i][z],relation[i][a], relation[i][b], relation[i][c], relation[i][d], relation[i][e]))
+                if len(three_set) == row:
+                    answer += 1
+                    havetocheck_list.append({x,y,z,a,b,c,d,e})
+    else:
+        return answer 
                 
 
 
